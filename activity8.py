b41b7a8b667e75ea3a90c7c1238d05013d267045
Arnulfo118 <arnulfo.official118@gmail.com>
2025-08-27 08:03:33 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,11 @@
+a = 5
+b = 5
+name1 = 'Arnulfo'
+name2 =  'Fernandez' 
+
+print( a>=b)
+print( name1 == name2)
+
+name1 += name2
+
+print(name1)
---
+++ activity9.py
@@ -0,0 +1,8 @@
+username = input("Enter username.. ")
+password = input("Enter password.. ")
+
+if (username == 'Arnulfo') and (password == 'arnulfo118'):
+    print("User Logged in")
+    print( (username == 'Arnulfo') and (password == 'arnulfo118') )
+else:
+    print("Wrong credentials")
