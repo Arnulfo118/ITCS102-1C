77470ce81df0fa35ce868b232a835c177f576d14
Arnulfo118 <arnulfo.official118@gmail.com>
2025-12-09 17:47:39 +0800

    Create activity23.py

---
+++ activity23.py
@@ -0,0 +1,10 @@
+from activity23_1 import *
+def funct():
+    print("Hello!")
+    print("Welcome to the Module")
+
+funct()   
+greetname('Arnulfo')
+greet('Dash', 'Lucena', '19')
+print(f"I want to get the summation of {funcwithReturn(10)}")
+print(f"The factorial of {fac(6)}")
